4fbbedcea7eaed6e47e10990ee4c67ec6159d80b
Michael <michael@MacBook-Air.local>
2022-03-14 16:15:15 -0400

    Add basic Python API

---
+++ main.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify
+from ediplug.ediplug import SmartPlug
+import os
+
+app = Flask(__name__)
+
+host = os.getenv('HOST')
+username = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
+
+ediplug = SmartPlug(host, (username, password))
+
+@app.route("/")
+@app.route("/info")
+def info():
+    return jsonify(device=ediplug.info, state=ediplug.state)
+
+@app.route("/status")
+def status():
+    return jsonify(state=ediplug.state)
+
+
+@app.route("/schedule")
+def schedule():
+    return jsonify(ediplug.schedule)
+
+
+@app.route("/toggle")
+def toggle():
+    requested_state = 'ON' if ediplug.state == 'OFF' else 'OFF'
+    ediplug.state = requested_state
+    return jsonify(state=ediplug.state)
+
+
+@app.route("/on")
+def power_on():
+    requested_state = 'ON'
+    ediplug.state = requested_state
+    return jsonify(state=ediplug.state)
+
+
+@app.route("/off")
+def power_off():
+    requested_state = 'OFF'
+    ediplug.state = requested_state
+    return jsonify(state=ediplug.state)
+
+@app.route("/usage")
+def usage():
+    return jsonify(current=ediplug.current, wattage=ediplug.power)
+
+
+
+if __name__ == '__main__':
+    app.run(port=4999)
